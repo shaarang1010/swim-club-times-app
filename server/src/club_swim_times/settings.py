7d13a8c3f0b31159c470d7a5ca53fac07a60009d
Shaarang Tanpure <shaarang.1910@gmail.com>
2022-10-24 23:06:01 +1100

    update Secret Key

--- server/src/club_swim_times/settings.py
+++ server/src/club_swim_times/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-rl)9$z@ozd-=p7!r1fngkt%uvq3a)*rgf71_e)hy*#t5&k*3j='
+SECRET_KEY = os.environ.get('APP_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
