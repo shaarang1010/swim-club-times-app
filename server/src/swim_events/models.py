ffc34f2735e0fc52cc7b334640cd81814a8d4770
Shaarang Tanpure <shaarang.1910@gmail.com>
2022-10-24 22:57:33 +1100

    add swim meet app

--- server/src/swim_events/models.py
+++ server/src/swim_events/models.py
@@ -2,6 +2,7 @@ from email.policy import default
 from pyexpat import model
 from django.db import models
 import uuid
+from swim_meet import SwimMeet
 
 # Create your models here.
 
---
+++ server/swim_meet/models.py
@@ -0,0 +1,13 @@
+from uuid import uuid4
+from django.db import models
+
+# Create your models here.
+
+
+class SwimMeet(models.Model):
+    id = models.UUIDField(default=uuid4, primary_key=True, editable=False)
+    name = models.CharField(max_length=255)
+    date = models.DateField(blank=True, null=True)
+    start_time = models.DateTimeField(blank=True, null=True)
+    end_time = models.DateTimeField(blank=True, null=True)
+    location = models.CharField(max_length=255)
