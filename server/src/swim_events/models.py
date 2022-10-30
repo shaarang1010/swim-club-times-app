7f5edbf65b602a36bcbb93c77b651a9aada6d124
Shaarang Tanpure <shaarang.1910@gmail.com>
2022-10-30 22:57:38 +1100

    update models

--- server/src/swim_events/models.py
+++ server/src/swim_events/models.py
@@ -9,10 +9,30 @@ from athlete_profile.models import AthleteProfile
 
 
 class SwimEvents(models.Model):
-    event_id = models.UUIDField(default=uuid.uuid4(), primary_key=True)
+    event = models.UUIDField(default=uuid.uuid4(), primary_key=True)
     name = models.CharField(max_length=100)
-    heat = models.IntegerField()
+    heats = models.IntegerField()
     time = models.DecimalField(max_digits=4, decimal_places=2)
     swim_meet = models.ForeignKey(SwimMeet, on_delete=models.RESTRICT)
-    athlete = models.ManyToManyField(
-        AthleteProfile, related_name='swimmer_event')
+
+    def __str__(self):
+        return self.name
+
+
+class SwimHeat(models.Model):
+    heat_no = models.IntegerField(default=1)
+    event = models.ForeignKey(SwimEvents, on_delete=models.RESTRICT)
+
+    def __str__(self):
+        return "Heat %s-%s" % (self.heat_no, self.event.name)
+
+
+class SwimmerEvent(models.Model):
+    comp = models.UUIDField(default=uuid.uuid4(), primary_key=True)
+    athlete = models.ForeignKey(AthleteProfile, on_delete=models.CASCADE)
+    event = models.ForeignKey(SwimEvents, on_delete=models.CASCADE)
+    heat = models.ForeignKey(SwimHeat, on_delete=models.RESTRICT)
+    swim_time = models.DecimalField(max_digits=5, decimal_places=4)
+
+    def __str__(self):
+        return f'{self.athlete.firstname}-{self.event.name}'
