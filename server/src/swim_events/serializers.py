8f70ac7661c9c04cc3d952c37be309812f6973d2
Shaarang Tanpure <shaarang.1910@gmail.com>
2022-11-01 21:35:22 +1100

    add serializers

---
+++ server/src/swim_events/serializers.py
@@ -0,0 +1,30 @@
+from dataclasses import fields
+from rest_framework import serializers
+from models import SwimEvent, SwimHeat, SwimmerEvent
+
+
+class SwimEventSerializers(serializers.ModelSerializer):
+    swim_meet = serializers.PrimaryKeyRelatedField(many=True, source='meet')
+
+    class Meta:
+        model = SwimEvent
+        fields = ["name", "heats", "time", "swim_meet"]
+
+
+class SwimHeatSerializer(serializers.ModelSerializer):
+    event = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+    heat_no = serializers.IntegerField()
+
+    class Meta:
+        model = SwimHeat
+        fields = '__all__'
+
+
+class SwimmerEventSerializer(serializers.ModelSerializer):
+    athelte = serializers.PrimaryKeyRelatedField()
+    event = serializers.PrimaryKeyRelatedField()
+    heat = serializers.PrimaryKeyRelatedField()
+
+    class Meta:
+        model = SwimmerEvent
+        fields = '__all__'
--- server/src/swim_meet/models.py
+++ server/src/swim_meet/models.py
@@ -8,6 +8,6 @@ class SwimMeet(models.Model):
     meet_id = models.UUIDField(default=uuid4, primary_key=True, editable=False)
     name = models.CharField(max_length=255)
     date = models.DateField(blank=True, null=True)
-    start_time = models.DateTimeField(blank=True, null=True)
-    end_time = models.DateTimeField(blank=True, null=True)
+    start_time = models.TimeField(blank=True, null=True)
+    end_time = models.TimeField(blank=True, null=True)
     location = models.CharField(max_length=255)
